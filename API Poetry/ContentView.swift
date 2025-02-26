9e5aa4779e871b3da90c67821ca6d977b94412fa
tyler0611 <berlity0611@gmail.com>
2025-02-26 09:40:32 -0600

    Setup ContentView

--- API Poetry/ContentView.swift
+++ API Poetry/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var authors = [String]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            List(authors, id: \.self) { author in
+                NavigationLink(destination: Text(author)) {
+                    Text(author)
+                }
+            }
+            .navigationTitle("Poetry Authors")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        .task {
+            await getAuthors()
+        }
+    }
+    
+    func getAuthors() async {
+        let query = "https://poetrydb.org//author"
+        if let url = URL(string: query) {
+            if let (data, _) = try? await URLSession.shared.data(from: url) {
+                if let decodedResponse = try? JSONDecoder().decode(Authors.self, from: data) {
+                    authors = decodedResponse.authors
+                }
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Authors: Codable {
+    var authors: [String]
 }
