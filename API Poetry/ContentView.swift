fe0f64725a589bf5968ec3051faaea541deb3182
tyler0611 <berlity0611@gmail.com>
2025-02-27 09:38:45 -0600

    Setup ContentView

--- API Poetry/ContentView.swift
+++ API Poetry/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var authors = [String]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView{
             List(authors, id: \.self) { author in
@@ -22,6 +23,11 @@ struct ContentView: View {
         .task {
             await getAuthors()
         }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the poetry authors"),
+                  dismissButton: .default(Text("OK")))
+        }
     }
     
     func getAuthors() async {
@@ -30,9 +36,11 @@ struct ContentView: View {
             if let (data, _) = try? await URLSession.shared.data(from: url) {
                 if let decodedResponse = try? JSONDecoder().decode(Authors.self, from: data) {
                     authors = decodedResponse.authors
+                    return
                 }
             }
         }
+        showingAlert = true
     }
 }
 
